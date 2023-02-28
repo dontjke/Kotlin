8fc4fa954213f85aa00185e3d8cef9b0268915c5
Romajke <skuaall@mail.ru>
2023-02-28 14:47:09 +0300

    добавил data class с свойствами, вывел на экран

---
+++ app/src/main/java/com/example/kotlin/Data.kt
@@ -0,0 +1,4 @@
+package com.example.kotlin
+
+data class Data(val title: String, val description: String)
+
--- app/src/main/java/com/example/kotlin/MainActivity.kt
+++ app/src/main/java/com/example/kotlin/MainActivity.kt
@@ -3,19 +3,28 @@ package com.example.kotlin
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 
         val button: Button = findViewById(R.id.button)
+        val title: TextView = findViewById(R.id.title_text_view)
+        val description: TextView = findViewById(R.id.description_text_view)
+
+
         button.setOnClickListener {
             Toast.makeText(this, "Hello Toast!", Toast.LENGTH_SHORT).show()
         }
 
+        "Заголовок".also { title.text = it }
+        description.text = "Описание"
 
     }
 }
