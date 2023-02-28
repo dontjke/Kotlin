3440ff57731342fc52979bcce2eafe64e1bae339
Romajke <skuaall@mail.ru>
2023-02-28 14:28:19 +0300

    добавил кнопку в Activity

--- app/src/main/java/com/example/kotlin/MainActivity.kt
+++ app/src/main/java/com/example/kotlin/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val button: Button = findViewById(R.id.button)
+        button.setOnClickListener {
+            Toast.makeText(this, "Hello Toast!", Toast.LENGTH_SHORT).show()
+        }
+
+
     }
 }
