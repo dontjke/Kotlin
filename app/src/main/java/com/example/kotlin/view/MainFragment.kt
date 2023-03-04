78f63884d7f4dd7df0458a92f5fbf3919959aa0c
Romajke <skuaall@mail.ru>
2023-03-04 21:19:26 +0300

    добавил фрагмент

---
+++ app/src/main/java/com/example/kotlin/view/MainFragment.kt
@@ -0,0 +1,31 @@
+package com.example.kotlin.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.kotlin.R
+
+class MainFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) = MainFragment()
+
+    }
+}
