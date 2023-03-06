8cf3703d6052506589108b1045d31ae1614a674c
Romajke <skuaall@mail.ru>
2023-03-06 14:13:44 +0300

    добавил binding

--- app/src/main/java/com/example/kotlin/view/main/MainFragment.kt
+++ app/src/main/java/com/example/kotlin/view/main/MainFragment.kt
@@ -5,22 +5,53 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.example.kotlin.R
+import com.example.kotlin.databinding.FragmentMainBinding
+import com.example.kotlin.viewmodel.MainViewModel
 
 class MainFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-        }
+    lateinit var binding: FragmentMainBinding //утечка памяти
+
+    override fun onDestroy() {
+        super.onDestroy()
+       // binding = null     след занятие
     }
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
+        binding = FragmentMainBinding.inflate(inflater, container, false)
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        // return inflater.inflate(R.layout.fragment_main, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonOne.setOnClickListener {}
+
+
+        val viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+        val observer = object : Observer<Any> {
+            override fun onChanged(data: Any) {
+                renderData(data)
+            }
+        }
+        viewModel.getData().observe(viewLifecycleOwner, observer)
+
+        viewModel.getWeather()
+    }
+
+    private fun renderData(data: Any) {
+        Toast.makeText(requireContext(), "Работает", Toast.LENGTH_SHORT).show()
     }
 
     companion object {
--- app/src/main/java/com/example/kotlin/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/kotlin/viewmodel/MainViewModel.kt
@@ -1,6 +1,21 @@
 package com.example.kotlin.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import java.lang.Thread.sleep
 
-class MainViewModel: ViewModel() {
+class MainViewModel(private val liveData: MutableLiveData<Any> = MutableLiveData()) : ViewModel() {
+
+    fun getData(): LiveData<Any>{
+        return liveData
+    }
+
+    fun getWeather(){
+        Thread{
+            sleep(1000)
+            liveData.postValue(Any())
+        }.start()
+
+    }
 }
