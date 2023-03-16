88aafd0d2069e637bb776d7ac672c4d18e143f34
Romajke <skuaall@mail.ru>
2023-03-16 16:53:22 +0300

    Подготовил новую MainActivityWebView

---
+++ app/src/main/java/com/example/kotlin/view/MainActivityWebView.kt
@@ -0,0 +1,17 @@
+package com.example.kotlin.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.kotlin.R
+import com.example.kotlin.view.weatherlist.WeatherListFragment
+
+class MainActivityWebView : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_web_view)
+
+
+    }
+}
