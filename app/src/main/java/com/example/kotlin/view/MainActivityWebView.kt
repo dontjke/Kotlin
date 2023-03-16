844291aa56a6944c4214d720c20fa37aa215285c
Romajke <skuaall@mail.ru>
2023-03-16 17:02:25 +0300

    добавил binding, кнопку. Надул макет

--- app/src/main/java/com/example/kotlin/view/MainActivityWebView.kt
+++ app/src/main/java/com/example/kotlin/view/MainActivityWebView.kt
@@ -3,15 +3,18 @@ package com.example.kotlin.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.kotlin.R
+import com.example.kotlin.databinding.ActivityMainWebViewBinding
 import com.example.kotlin.view.weatherlist.WeatherListFragment
 
 class MainActivityWebView : AppCompatActivity() {
-
-
+    lateinit var binding: ActivityMainWebViewBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main_web_view)
+        binding = ActivityMainWebViewBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.ok.setOnClickListener {
 
+        }
 
     }
 }
