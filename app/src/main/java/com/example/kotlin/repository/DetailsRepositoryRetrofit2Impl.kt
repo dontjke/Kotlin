d53279a6c33f20565ac7cccbddb506eb92cb5c1f
Romajke <skuaall@mail.ru>
2023-03-23 20:39:25 +0300

    добавил DetailsRepositoryRetrofit2Impl и подключил в манифесте

---
+++ app/src/main/java/com/example/kotlin/repository/DetailsRepositoryRetrofit2Impl.kt
@@ -0,0 +1,9 @@
+package com.example.kotlin.repository
+
+import com.example.kotlin.viewmodel.DetailsViewModel
+
+class DetailsRepositoryRetrofit2Impl:DetailsRepository {
+    override fun getWeatherDetails(city: City, callback: DetailsViewModel.Callback) {
+
+    }
+}
