4267ee46a6011edadf996c304199257db857686d
Romajke <skuaall@mail.ru>
2023-03-31 20:59:57 +0300

    добавил обетртку для макета с картой (поиск/ввод адреса)

--- app/src/main/java/com/example/kotlin/lesson10/MapsFragment.kt
+++ app/src/main/java/com/example/kotlin/lesson10/MapsFragment.kt
@@ -1,21 +1,32 @@
 package com.example.kotlin.lesson10
 
-import androidx.fragment.app.Fragment
-
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.example.kotlin.R
-
+import com.example.kotlin.databinding.FragmentMapsMainBinding
 import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
 class MapsFragment : Fragment() {
+
+    private var _binding: FragmentMapsMainBinding? = null //убрали утечку памяти
+    private val binding: FragmentMapsMainBinding
+        get() {
+            return _binding!!
+        }
+
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
 
     private val callback = OnMapReadyCallback { googleMap ->
         /**
@@ -36,8 +47,9 @@ class MapsFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_maps, container, false)
+    ): View {
+        _binding = FragmentMapsMainBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
